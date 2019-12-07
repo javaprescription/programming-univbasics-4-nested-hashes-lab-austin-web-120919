518e74bfa3d5fa57fc0114a4d6e154e97877a765
Austin Scott <austinscott4@gmail.com>
2019-12-07 17:54:23 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -85,8 +85,6 @@ def adding_matz
   hash2.merge(programmer_hash)
 end
 
-
-
 def changing_alan
 	programmer_hash =
  		{
@@ -105,8 +103,7 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
      alans_new_info = "GUI"
-
-
+     programmer_hash[alan_kay][known_for]=alans_new_info
 end
 
 def adding_to_dennis
